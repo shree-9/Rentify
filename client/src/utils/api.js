f52b00453fedaaa41d17279b12bfe63b574399d9
shree-9 <113379335+shree-9@users.noreply.github.com>
2025-10-05 23:08:46 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://rentify-gamma-azure.vercel.app/api",
 });
 
 
@@ -196,4 +196,5 @@ export const createResidency = async (data, token) => {
   {
     throw error
   }
+
 }
