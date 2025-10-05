77210e3da173b88cc6667064cf8e0b38b7e6d701
shree-9 <113379335+shree-9@users.noreply.github.com>
2025-10-05 23:56:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,28 +2,30 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import { userRoute } from './routes/userRoute.js';
+import {userRoute } from './routes/userRoute.js';
 import { residencyRoute } from './routes/residencyRoute.js';
 
 dotenv.config()
 
 const app = express();
 
-const PORT = process.env.PORT || 3000; 
 
-app.use(express.json());
-app.use(cookieParser());
 
-app.use(cors({
-  origin: "https://rentify-main.vercel.app", // frontend domain
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
-// Mount your API routes
-app.use("/api/user", userRoute);
-app.use("/api/residency", residencyRoute);
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+app.use("/api/user",userRoute)
+app.use("/api/residency", residencyRoute)   
+
+
+
 
